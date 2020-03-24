b0e8cef523fae80da88a8898f82d3d03d567d15f
Борис Цейтлин <euphetar@gmail.com>
2020-03-24 18:07:22 +0300

    WTF form, create view

--- cowork_site/candidates/views.py
+++ cowork_site/candidates/views.py
@@ -1,16 +1,22 @@
 from flask.views import View
-from flask import render_template
-from flask import current_app
+from flask import current_app, request, render_template, redirect, flash, url_for
 
 from cowork_site.models import Candidate
+from .forms import CandidateCreateForm
 
 
-class CandidateListView(View):
+class BaseView(View):
+
+    template_name = None
+
+    def render_template(self, **kwargs):
+
+        return render_template(self.template_name, **kwargs)
+
+
+class CandidateListView(BaseView):
 
     template_name = 'list.html'
-
-    def render_template(self, context):
-        return render_template(self.template_name, **context)
 
     def get_objects(self):
         s = current_app.session_factory()
@@ -18,5 +24,29 @@ class CandidateListView(View):
         return objects
 
     def dispatch_request(self):
-        context = {'objects': self.get_objects()}
-        return self.render_template(context)
+        return self.render_template(objects=self.get_objects())
+
+
+class CandidateCreateView(BaseView):
+
+    template_name = 'create.html'
+
+    def dispatch_request(self):
+        s = current_app.session_factory()
+
+        form = CandidateCreateForm()
+        if form.validate_on_submit():
+            candidate = Candidate(
+                name=form.name.data,
+                email=form.email.data,
+                description=form.description.data,
+                cv_url=form.cv_url.data,
+            )
+            s.add(candidate)
+            s.commit()
+            flash('Добавлено')
+            return redirect(url_for('candidates.candidate_list'))
+
+        return self.render_template(form=form)
+
+
