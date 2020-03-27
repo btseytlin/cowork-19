482ee6457854c07a26bd3e44aac4b742fc0d957e
Борис Цейтлин <euphetar@gmail.com>
2020-03-27 14:09:50 +0300

    pagination

--- cowork_site/postings/blueprint.py
+++ cowork_site/postings/blueprint.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from .views import PostingListView, PostingCreateView
+from .views import PostingListView, PostingCreateView, PostingArchiveView
 
 
 postings_blueprint = Blueprint('postings', __name__)
@@ -7,3 +7,5 @@ postings_blueprint = Blueprint('postings', __name__)
 postings_blueprint.add_url_rule('/', view_func=PostingListView.as_view('posting_list'), methods=['GET'])
 postings_blueprint.add_url_rule('/create', view_func=PostingCreateView.as_view('posting_create'),
                                   methods=['POST', 'GET'])
+postings_blueprint.add_url_rule('/archive/<posting_id>', view_func=PostingArchiveView.as_view('posting_archive'),
+                                  methods=['GET'])
