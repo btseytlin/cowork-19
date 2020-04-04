8e3bf59cf26fcbaa89651036e06e336ff595b989
Борис Цейтлин <euphetar@gmail.com>
2020-04-04 11:40:45 +0300

    Add welcome

--- cowork_site/static/main.js
+++ cowork_site/static/main.js
@@ -5,3 +5,66 @@ function confirm_delete(post_id){
       window.location.href = "/archive/"+post_id
     }
 }
+
+
+function createCookie(name,value,days) {
+	if (days) {
+		var date = new Date();
+		date.setTime(date.getTime()+(days*24*60*60*1000));
+		var expires = "; expires="+date.toGMTString();
+	}
+	else var expires = "";
+	document.cookie = name+"="+value+expires+"; path=/";
+}
+
+function readCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
+}
+
+function eraseCookie(name) {
+	createCookie(name,"",-1);
+}
+
+
+function isWelcomeHidden(){
+    if (readCookie('welcome-hidden') != null) {
+        return true;
+    }
+    return false;
+}
+
+function showWelcome(){
+    const welcome_elem = document.getElementById('welcome');
+    if (welcome_elem) {
+        welcome_elem.classList.remove("hidden");
+    }
+}
+
+function hideWelcome(){
+    const welcome_elem = document.getElementById('welcome');
+    if (welcome_elem) {
+        welcome_elem.classList.add("hidden");
+    }
+}
+
+
+function closeWelcome(){
+    createCookie('welcome-hidden', '1', 999)
+    hideWelcome();
+}
+
+
+
+window.onload = function(){
+    const welcome_hidden = isWelcomeHidden();
+    if (welcome_hidden === false) {
+        showWelcome()
+    }
+}
