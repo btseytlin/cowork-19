53b6d463bdc64206dd83cd9363fecc40ef13cbaa
Борис Цейтлин <euphetar@gmail.com>
2020-04-21 12:57:55 +0300

    Add b2b listings

--- cowork_site/static/main.js
+++ cowork_site/static/main.js
@@ -1,7 +1,6 @@
 function confirm_delete(post_id){
     const r = confirm("Точно удалить?");
     if (r === true) {
-        console.log(post_id)
       window.location.href = "/archive/"+post_id
     }
 }
